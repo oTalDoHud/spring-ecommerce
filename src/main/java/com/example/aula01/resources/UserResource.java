eb813a4ba5f824c9520a3bfa118ded581390d27b
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-31 10:27:21 -0300

    Serviço e controller - findById e findAll

--- src/main/java/com/example/aula01/resources/UserResource.java
+++ src/main/java/com/example/aula01/resources/UserResource.java
@@ -1,20 +1,38 @@
 package com.example.aula01.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.aula01.model.entities.User;
+import com.example.aula01.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
-	@GetMapping()
-	public ResponseEntity<User> findAll(){
-		User user = new User("Hudson", "Hudson@gmail.com"
-				, "11932165703", "HudsonLucas2000");
-		return ResponseEntity.ok().body(user);
+	@GetMapping(path = "/all")
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> users = service.findAll();
+		
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(path = "/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id) {
+		
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 }
---
+++ src/main/java/com/example/aula01/services/UserService.java
@@ -0,0 +1,26 @@
+package com.example.aula01.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aula01.model.entities.User;
+import com.example.aula01.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	public User findById(Integer id) {
+		Optional<User> user =  userRepo.findById(id);
+		return user.get();
+	}
+	
+	public List<User> findAll(){
+		return userRepo.findAll();
+	}
+}
