4c404a7acd79b70de53617617cc3fc05d0bd6910
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-30 23:44:52 -0300

    H2 banco teste e jpa

--- src/main/java/com/example/aula01/model/entities/User.java
+++ src/main/java/com/example/aula01/model/entities/User.java
@@ -2,18 +2,32 @@ package com.example.aula01.model.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Users")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(name = "Nomes")
 	private String name;
 	
+	@Column(name = "Email", unique = true)
 	private String email;
 	
+	@Column(name = "Telefones")
 	private String phone;
 	
+	@Column(name = "Senhas")
 	private String password;
 
 	public User() {
