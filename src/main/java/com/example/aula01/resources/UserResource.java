c3e98a888587221f78fee80bc0bd8d67e1f5396f
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-31 09:46:11 -0300

    -BugFix- Retirei construtor de user com id

--- src/main/java/com/example/aula01/resources/UserResource.java
+++ src/main/java/com/example/aula01/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 
 	@GetMapping()
 	public ResponseEntity<User> findAll(){
-		User user = new User(1, "Hudson", "Hudson@gmail.com"
+		User user = new User("Hudson", "Hudson@gmail.com"
 				, "11932165703", "HudsonLucas2000");
 		return ResponseEntity.ok().body(user);
 	}
