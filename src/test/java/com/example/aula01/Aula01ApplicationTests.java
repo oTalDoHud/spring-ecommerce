8812eaa95abfd2f4bd6e1f034c37d6937507e3e8
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-30 22:22:26 -0300

    Projeto criado

---
+++ src/test/java/com/example/aula01/Aula01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
