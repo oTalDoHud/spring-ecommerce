4b9f1235795f7221868f7353bc877a50c7cbe9a2
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-02 22:24:42 -0300

    Entidade de id composto Order item pt01

---
+++ src/main/java/com/example/aula01/model/entities/OrderItem.java
@@ -0,0 +1,98 @@
+package com.example.aula01.model.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.example.aula01.model.entities.pk.OrderItemPk;
+
+@Entity
+@Table(name = "Tb_Order_Item")
+public class OrderItem implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPk id;
+	
+	@Column(name = "Quantidades")
+	private Integer quantity;
+	
+	@Column(name = "Precos")
+	private Double price;
+
+	public OrderItem() {
+
+	}
+	
+	public OrderItem(Integer quantity, Double price) {
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
