55a53d011e490beb6abbf984e33c2bf85792f6ef
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-06 09:23:48 -0300

    Execption handler - delete

--- src/main/java/com/example/aula01/services/UserService.java
+++ src/main/java/com/example/aula01/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.aula01.model.entities.User;
 import com.example.aula01.repositories.UserRepository;
+import com.example.aula01.services.exceptions.DatabaseException;
 import com.example.aula01.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,11 +34,19 @@ public class UserService {
 	}
 	
 	public User deleteById(Integer id) {
-		Optional<User> user = userRepo.findById(id);
-		
-		userRepo.deleteById(id);
-		
-		return user.get();
+		try {
+			Optional<User> user = userRepo.findById(id);
+			
+			userRepo.deleteById(id);
+			
+			return user.get();
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User updateUser(Integer id, User user) {
---
+++ src/main/java/com/example/aula01/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.example.aula01.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+}
